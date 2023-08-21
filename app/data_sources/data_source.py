53a7be5a8e0074a784b9a826a7996039a4b9a3f4
Julia Raucci <jraucci@google.com>
2023-08-21 21:19:12 +0000

    Fixing bugs4

--- app/data_sources/data_source.py
+++ app/data_sources/data_source.py
@@ -6,14 +6,12 @@ class SourceType(Enum):
     BIG_QUERY = range(1)
 
 class DataSource():
-    def __init__(self, source_type: SourceType, db_authentication: Optional[dict], db_options: Optional[dict]):
+    def __init__(self, source_type: SourceType):
         self._source_type = source_type
-        self._db_authentication = db_authentication
-        self._db_options = db_options
 
     def get_data_source(self):
         if self._source_type == SourceType.BIG_QUERY:
-            return BigQueryDataSource(self._db_authentication, self._db_options)
+            return BigQueryDataSource()
         else:
             raise NotImplementedError("Source Type not implemented. Please check your configuration.")
         return data_source
